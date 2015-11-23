942d6f6249094a474357edadb4730c8973337617
Your Full Name <Your EMail Id>
2015-11-23 15:51:44 +0530

    pearson's correlation coefficient function is added

--- Programmers Guide datamining.py
+++ Programmers Guide datamining.py
@@ -111,3 +111,86 @@ def minkowski_closest_person (u_name , users , r):
             closest_list.append((distance, user))
     return sorted (closest_list)
 
+
+
+# karl-pearsons correlation coefficient.
+
+def xbar (dic1, dic2):
+    size = 0
+    sum1 = 0
+    for key in dic1:
+        if key in dic2:
+            sum1 += dic1[key]
+            size += 1
+    return sum1/size
+def ybar (dic1, dic2):
+    size = 0
+    sum1 = 0
+    for key in dic1:
+        if key in dic2:
+            sum1 += dic2[key]
+            size += 1
+    return sum1/size
+
+def pearson_corr(data1 , data2):
+    cov_xy  = 0
+    sigma_x = 0
+    sigma_y = 0
+    for key in data1:
+        if key in data2:
+            cov_xy += ((data1[key] - xbar(data1,data2)) * (data2[key] - ybar(data1, data2)))
+            sigma_x   += pow((data1[key] - xbar(data1, data2)), 2)
+            sigma_y   += pow((data2[key] - ybar(data1, data2)), 2)
+    return cov_xy/(sqrt(sigma_x) * sqrt(sigma_y))
+'''
+Output
+
+>>> pearson_corr(users['Angelica'], users['Bill'])
+-0.9040534990682688
+
+>>> pearson_corr(users['Angelica'], users['Hailey'])
+0.42008402520840293
+
+>>> pearson_corr(users['Angelica'], users['Jordyn'])
+0.7639748605475432
+
+'''
+
+def pearson (ratings1, ratings2):
+    sum_xy = 0
+    sum_x = 0
+    sum_y = 0
+    sum_x2 = 0
+    sum_y2 = 0
+    n = 0
+    for key in ratings1:
+        if key in ratings2:
+            n += 1
+            x = ratings1[key]
+            y = ratings2[key]
+            sum_x += x
+            sum_y += y
+            sum_xy += x*y
+            sum_x2 += x**2
+            sum_y2 += y**2
+    denominator = sqrt (sum_x2 - (sum_x ** 2)/n) * sqrt (sum_y2 - (sum_y ** 2)/n)
+    if n == 0:
+        return 0
+    if denominator == 0:
+        return 0
+    else:
+        return (sum_xy-(sum_x * sum_y) / n)/ denominator
+
+'''
+
+>>> pearson(users['Angelica'], users['Bill'])
+-0.9040534990682699
+
+>>> pearson(users['Angelica'], users['Hailey'])
+0.42008402520840293
+
+>>> pearson(users['Angelica'], users['Jordyn'])
+0.7639748605475432
+
+'''
+
